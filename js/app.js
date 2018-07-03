e089bf465510152b6afd18fbf4089c031ceb8495
Zak Laughton <zlaughton@gmail.com>
2018-07-02 23:34:30 -0500

    docs: removed helper comments

--- js/app.js
+++ js/app.js
@@ -93,19 +93,6 @@ class Bug extends Character {
 const player = new Player();
 const allEnemies = [new Bug(0, 1, 'right'), new Bug(5, 2, 'left'), new Bug(0, 3, 'right')]
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
